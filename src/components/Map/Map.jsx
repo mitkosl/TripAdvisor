8f04b8ca8f1e20756041e81b8ae0de98d5e03e0d
Dimitar Slavchev <mitkosl@abv.bg>
2023-04-05 23:25:06 +0300

    Added google maps, with unknown error yet...

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,6 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import { LocationOnOutlined } from '@material-ui/icons';
 import { Rating } from '@material-ui/lab';
+import { useLoadScript } from 'google-map-react'
 
 import useStyles from './styles'
 
@@ -11,20 +12,29 @@ const Map = () => {
     const isMobile = useMediaQuery('(min-width: 600px)');
     const coordinates = { lat: 0, long: 0 };
 
+    // const { isLoading } = useLoadScript({
+    //     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    // });
+
+    // if(!isLoading) return <div>Loading...</div>
+
+    // return <div>Maps</div>
+
     return(
-    <div className={classes.mapContainer}>
-        <GoogleMapReact
-            bootstrapURLkeys={{ key: '' }}
-            defaultCenter={coordinates}
-            center={coordinates}
-            defaultZoom={14}
-            margin={[50, 50, 50, 50]}
-            options={''}
-            onChange={''}
-            onChildClick={''}>
-        </GoogleMapReact>
-    </div>
-        )
+        <div id="Map" className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLkeys={{ key: 'AIzaSyDgAeHRRNSqZTKK69FjG3CYtA8PplzfAag' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+                >
+            </GoogleMapReact>
+        </div>
+    );
 } 
 
 export default Map;
